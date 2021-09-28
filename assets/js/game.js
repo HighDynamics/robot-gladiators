1473537b2d118e4202ad2d1ab2df106f38464e3b
Daniel Denton <danielryandenton@gmail.com>
2021-09-27 22:12:22 -0500

    add lose conditionals

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,7 +5,7 @@ var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyAttack = 82;
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
 //  * Fight all enemy-robots
@@ -27,7 +27,7 @@ var fight = function (enemyName) {
 
       // If yes (true), leave fight
       if (confirmSkip) {
-        window.alert(playerName + " has chosen to skp the fight! Goodbye!");
+        window.alert(playerName + " has chosen to skip the fight! Goodbye!");
         // Subtract money from playerMoney for skipping
         playerMoney = playerMoney - 10;
         console.log("playerMoney", playerMoney);
@@ -88,8 +88,23 @@ var fight = function (enemyName) {
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  // Call fight function with enemy-robot
-  fight(pickedEnemyName);
+  if (playerHealth > 0) {
+      // Let player know what round they are in, remember that arrays start at 0, so it needs to have 1 added to it
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+    // Pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+
+    // Reset enemyHealth before starting new fight
+    enemyHealth = 50;
+
+    // Use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+
+    // Call fight function with enemy-robot
+    fight(pickedEnemyName);
+  } else {
+      window.alert("You have lost your robot in the battle! Game over!");
+      break;
+  }
 }
